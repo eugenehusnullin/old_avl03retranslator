3718615baf75fce74dd62aec06c943a1a8aabeb5
bob <v.husnullin@gmail.com>
2013-04-02 02:10:36 -0700

    Mintrans fixes

--- TcpServer.Core/Mintrans/SoapSink.cs
+++ TcpServer.Core/Mintrans/SoapSink.cs
@@ -8,42 +8,43 @@ namespace TcpServer.Core.Mintrans
     public class SoapSink
     {
         private SoapSinkSettings settings;
-        private ObjectPool<WebRequest> pool;
 
         public SoapSink(SoapSinkSettings settings)
         {
             this.settings = settings;
-            this.pool = new ObjectPool<WebRequest>(40, () => WebRequest.Create(this.settings.Url));
         }
 
         public async Task PostSoapMessage(byte[] message)
         {
             await Task.Run(() =>
             {
-                WebRequest request = this.pool.GetFromPool();
+                HttpWebRequest request = WebRequest.Create(this.settings.Url) as HttpWebRequest;
                 try
                 {
                     request.Method = "POST";
+                    request.KeepAlive = true;
                     request.ContentType = "application/soap+xml";
                     request.ContentLength = message.Length;
                     request.Credentials = new NetworkCredential(this.settings.UserName, this.settings.Password);
                     using (Stream stream = request.GetRequestStream())
                     {
                         stream.Write(message, 0, message.Length);
-                        WebResponse response = request.GetResponse();
-                        if ((response as HttpWebResponse).StatusCode == HttpStatusCode.OK)
+                        HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                        if (response.StatusCode == HttpStatusCode.OK)
                         {
                             using (Stream responseStream = response.GetResponseStream()) { }
+                        }
+                        else
+                        {
+                            throw new ApplicationException("PostSoapMessage: " + response.StatusDescription);
                         }
                     }
 
                     request.Abort();
-                    this.pool.ReturnToPool(request);
                 }
                 catch
                 {
                     request.Abort();
-                    this.pool.DestroyOne();
                     throw;
                 }
             });
