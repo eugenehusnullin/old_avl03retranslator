5392d484aa65928f9e7868338bc39843941d1b6c
Eugene Husnullin <ehusnullin80@gmail.com>
2013-08-12 18:26:08 +0300

    Имя сервиса в конфиге.

--- TcpServer.Retranslator.Service/ProjectInstaller.cs
+++ TcpServer.Retranslator.Service/ProjectInstaller.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Reflection;
 
 namespace TcpServer.Retranslator.Service
 {
@@ -8,6 +11,25 @@ namespace TcpServer.Retranslator.Service
         public ProjectInstaller()
         {
             InitializeComponent();
+
+            GlonassInstaller.ServiceName = GetConfigurationValue("ServiceName");
+            GlonassInstaller.Description = GetConfigurationValue("ServiceName");
+            GlonassInstaller.DisplayName = GetConfigurationValue("ServiceName");
+        }
+
+        private string GetConfigurationValue(string key)
+        {
+            Assembly service = Assembly.GetAssembly(typeof(GlonassAsync));
+            Configuration config = ConfigurationManager.OpenExeConfiguration(service.Location);
+            if (config.AppSettings.Settings[key] != null)
+            {
+                return config.AppSettings.Settings[key].Value;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException
+                    ("Settings collection does not contain the requested key: " + key);
+            }
         }
     }
 }
