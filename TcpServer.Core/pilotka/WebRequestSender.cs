70f6c0255de5bd49799508bcd27a4fc6cc788022
Eugene Husnullin <ehusnullin80@gmail.com>
2014-02-21 16:33:28 +0200

    activemq added

--- TcpServer.Core/pilotka/WebRequestSender.cs
+++ TcpServer.Core/pilotka/WebRequestSender.cs
@@ -1,4 +1,6 @@
-﻿using log4net;
+﻿using Apache.NMS;
+using Apache.NMS.Util;
+using log4net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +8,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using TcpServer.Core.Properties;
 
 namespace TcpServer.Core.pilotka
 {
@@ -16,6 +19,10 @@ namespace TcpServer.Core.pilotka
         private ILog log;
         private string datetimeFormat = "dd.MM.yyyy HH:mm:ss";
         private int webRequestTimeout = 10000;
+
+        private ISession session = null;
+        private IMessageProducer producer = null;
+        private IConnection connection = null;
 
         public WebRequestSender(PilotkaSettings settings)
         {
@@ -43,6 +50,12 @@ namespace TcpServer.Core.pilotka
                             var webResponse = (HttpWebResponse)webRequest.GetResponse();
                             log.DebugFormat("WebRequestSender: webResponse = {0}, url={1}", webResponse.StatusCode, currentUrl);
 
+                            // active mq post message
+                            if (Settings.Default.Activemq_enabled)
+                            {
+                                nsmSend(imei, state, utcDatetime);
+                            }
+
                             return webResponse.StatusCode == HttpStatusCode.OK;
                         }
                         catch (Exception e)
@@ -58,6 +71,68 @@ namespace TcpServer.Core.pilotka
                     }
                 }
             );
+        }
+
+        private void createConnection()
+        {
+            Uri connectUri = new Uri(Settings.Default.Activemq_url);
+            var factory = new NMSConnectionFactory(connectUri);
+            connection = factory.CreateConnection(Settings.Default.Activemq_user, Settings.Default.Activemq_password);
+            connection.Start();
+            session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            var queue = SessionUtil.GetQueue(session, Settings.Default.Activemq_queue);
+            producer = session.CreateProducer(queue);
+            producer.DeliveryMode = MsgDeliveryMode.Persistent;
+        }
+
+        private void closeConnection()
+        {
+            try
+            {
+                producer.Close();
+            }
+            catch { }
+
+            try
+            {
+                session.Close();
+            }
+            catch { }
+
+            try
+            {
+                connection.Stop();
+            }
+            catch { }
+
+            try
+            {
+                connection.Close();
+            }
+            catch { }
+
+            producer = null;
+            session = null;
+            connection = null;
+        }
+
+        private bool nsmSend(string imei, PilotkaState state, DateTime utcDatetime)
+        {
+            try
+            {
+                if (session == null)
+                {
+                    createConnection();
+                }
+
+                string msg = imei + "|" + (state == PilotkaState.Started ? "1" : "0") + "|" + utcDatetime.ToString(datetimeFormat);
+                var textMessage = session.CreateTextMessage(msg);
+                producer.Send(textMessage);
+                return true;
+            } catch {
+                closeConnection();
+                return false;
+            }
         }
     }
 }
